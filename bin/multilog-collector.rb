48c3a48e879b32475b91c49ef86dd3d73572ba45
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2011-01-12 21:32:46 +0900

    add REAME and parse options

--- bin/multilog-collector.rb
+++ bin/multilog-collector.rb
@@ -1,4 +1,14 @@
 #!/usr/bin/env ruby
+require 'optparse'
+
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 require 'djb/daemontools/multilog/collector'
-puts Djb::Daemontools::Multilog::Collector.new.collect
+
+directory = "/service"
+
+opt = OptionParser.new
+opt.on('--help', 'show this message') { puts opt; exit }
+opt.on('-d DIR','--directory=DIR','service directory default: /service'){ |v| directory = v }
+opt.parse!(ARGV)
+
+puts Djb::Daemontools::Multilog::Collector.new(directory).collect
