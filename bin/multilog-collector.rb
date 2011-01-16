12a862f3e7eb507a726ef47a13a76c2c7ea48918
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2011-01-16 17:53:49 +0900

    add -l options refactor and split class files

--- bin/multilog-collector.rb
+++ bin/multilog-collector.rb
@@ -5,10 +5,13 @@ $:.unshift(File.dirname(__FILE__) + '/../lib')
 require 'djb/daemontools/multilog/collector'
 
 directory = "/service"
+collector = Djb::Daemontools::Multilog::Collector.new
 
 opt = OptionParser.new
 opt.on('--help', 'show this message') { puts opt; exit }
-opt.on('-d DIR','--directory=DIR','service directory default: /service'){ |v| directory = v }
+opt.on('-d DIR','--directory=DIR','service directory default: /service'){ |v| collector.directory = v }
+opt.on('-l N','--lines=N','lines to collect default: 100'){ |v| collector.lines = v.to_i }
 opt.parse!(ARGV)
 
-puts Djb::Daemontools::Multilog::Collector.new(directory).collect
+puts collector.collect
+
--- lib/djb/daemontools/multilog/collector.rb
+++ lib/djb/daemontools/multilog/collector.rb
@@ -1,54 +1,15 @@
 #!/usr/bin/env ruby
-# vim:set fileencoding=utf-8:
-
-require 'djb/tai64n'
+# -*- coding: utf-8 -*-
+require 'djb/daemontools/multilog/logs'
 module Djb
   module Daemontools
     module Multilog
-      class Logs < Array
-        def load(file)
-          @file = file
-          open(file) do |f|
-            while line = f.gets
-              self << Log.new(line,service)
-            end
-          end
-          self
-        end
-
-        def service
-          $1 if /\/([^\/]+)\/log\/main\/current/ =~ @file
-        end
-
-        def to_s
-          join("\n",self)
-        end
-
-      end
-
-      class Log 
-        def initialize(line,service)
-          @line = line
-          @service = service
-        end
-
-        def tai64n
-          return $1 if /^(@[a-z0-9]+) / =~ @line
-        end
-
-        def time
-          Tai64n::to_local(tai64n)
-        end
-
-        def to_s
-          if /^(@[a-z0-9]+) (.*)/ =~ @line
-            "%s [%s] %s" % [time.strftime("%Y-%m-%d %H:%M:%S"),@service,$2]
-          end
-        end
-      end
-
       class Collector
+        attr_accessor :service_dir,:lines
+        alias directory= service_dir=
+
         def initialize(dir = "/service")
+          @lines = 100
           @service_dir = dir
         end
 
@@ -56,20 +17,19 @@ module Djb
           all = Logs.new
           glob_pattern = @service_dir + "/*/log/main/current"
           Dir.glob(glob_pattern).each do |file| 
-            all += Logs.new.load(file)
+            all += Logs.new.load(file,@lines)
           end
-          all.sort do |a,b|
+          all.sort! do |a,b|
             a.time <=> b.time
           end
+          all[-@lines..-1]
         end
-        
-        
       end
       
     end
   end
 end
 =begin
-# collector = Djb::Daemontools::Multilog::Collector.new("/Users/tumf/service")
-# puts collector.collect
+#collector = Djb::Daemontools::Multilog::Collector.new("/Users/tumf/service")
+#puts collector.collect
 =end
