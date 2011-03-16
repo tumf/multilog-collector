c6ece1fc808e7ea889b5be8c0aca11f53a37d35d
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2011-03-16 09:17:02 -0700

    Edited test/helper.rb via GitHub

--- test/helper.rb
+++ test/helper.rb
@@ -12,7 +12,7 @@ require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'multilog-collector'
-
+require 'optparse'
+require 'djb/daemontools/multilog/collector'
 class Test::Unit::TestCase
 end
