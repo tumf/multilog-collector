882ee9d797d03c392cf94d52b7dc6bf224044d8b
高原芳浩 <y.takahara@gmail.com>
2014-06-02 07:44:07 +0900

    add UTC

--- spec/multilog-collector_spec.rb
+++ spec/multilog-collector_spec.rb
@@ -43,8 +43,8 @@ end
 
 describe Djb::Tai64n do
   describe ".to_local" do
-    it "returns 2011-03-17 20:01:46.923072500" do
-      expect(Djb::Tai64n::to_local("@400000004d81ea243704f7f4")).to eq Time.parse("2011-03-17 20:01:46.923072500").gmtime
+    it "returns 2011-03-17 20:01:46.923072500 UTC" do
+      expect(Djb::Tai64n::to_local("@400000004d81ea243704f7f4")).to eq Time.parse("2011-03-17 11:01:46.923072500 UTC")
     end
   end
 end
