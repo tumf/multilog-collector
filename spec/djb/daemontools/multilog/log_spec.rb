308f04ba6212b57d0346bb639a0a14ede9b1d0c7
高原芳浩 <y.takahara@gmail.com>
2014-06-02 09:00:06 +0900

    fix: rspec

--- spec/djb/daemontools/multilog/log_spec.rb
+++ spec/djb/daemontools/multilog/log_spec.rb
@@ -7,7 +7,7 @@ describe Djb::Daemontools::Multilog::Log do
   describe "#to_s" do
     it "returns log as string" do
       @log = Djb::Daemontools::Multilog::Log.new("@400000004d81ed7004a435a4 query 9668 7f000001:e9cd:0b87 1 github.com.","test-service")
-      expect(@log.to_s).to eq "2011-03-17 20:15:50 [test-service] query 9668 7f000001:e9cd:0b87 1 github.com."
+      expect(@log.to_s).to match /\[test-service\] query 9668 7f000001:e9cd:0b87 1 github.com\./
     end
   end
 end
